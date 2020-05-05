481e8db8ad2e2504e42c925649d42d602be247b3
Josiah  Partridge <jpartridj94@gmail.com>
2020-05-05 02:42:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -48,7 +48,9 @@ def kesha_maker(string_array)
 end
 
 def find_a(string_array)
-  string_array.map {|string| string.start_with?("a")}
+  string_array.map {|string| string.start_with?("a")
+    
+  }
 end
 
 def sum_array(int_array)
