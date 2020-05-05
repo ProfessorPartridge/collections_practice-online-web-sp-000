50d5664730342488275f5580cad3c82f8ee18cb0
Josiah  Partridge <jpartridj94@gmail.com>
2020-05-05 01:52:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
+def sort_array_asc(int_array)
+  int_array.sort()
+end
 
+def sort_array_desc(int_array)
+  int_array.sort do |int_1, int_2|
+    if int_1 == int_2
+      0
+    elsif int_1 < int_2
+      1
+    elsif int_1 > int_2
+      -1 
+    end
+  end
+end
+
+def sort_array_char_count(string_array)
+  string_array.sort do |string_1, string_2|
+    
+end
